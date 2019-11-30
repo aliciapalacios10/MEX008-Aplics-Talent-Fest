3ab27ab49a37c0a9c4cd72f8fd59837e27d690d8
Viviana <vivianaberron@gmail.com>
2019-11-30 13:10:15 -0600

    Updates

---
+++ precache-manifest.f8dbaa957eac0d15f40f70918340dba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa1821980a36ed9e5312b8f5a6db30a1",
+    "url": "/vivianaberron/MEX008-Aplics-Talent-Fest/index.html"
+  },
+  {
+    "revision": "e9995bdb1b6e534ce8dd",
+    "url": "/vivianaberron/MEX008-Aplics-Talent-Fest/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5a01b6df94b3c281b845",
+    "url": "/vivianaberron/MEX008-Aplics-Talent-Fest/static/js/2.6441c26b.chunk.js"
+  },
+  {
+    "revision": "e9995bdb1b6e534ce8dd",
+    "url": "/vivianaberron/MEX008-Aplics-Talent-Fest/static/js/main.c62eb35a.chunk.js"
+  },
+  {
+    "revision": "d8082e54a4573f689580",
+    "url": "/vivianaberron/MEX008-Aplics-Talent-Fest/static/js/runtime-main.534f7dad.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vivianaberron/MEX008-Aplics-Talent-Fest/static/media/logo.25bf045c.svg"
+  }
+]);

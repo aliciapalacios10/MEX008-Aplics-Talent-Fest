1e0fa1a9053fb5d4d1ccf0a7a45cfc2430294dff
Viviana <vivianaberron@gmail.com>
2019-12-03 02:37:52 -0600

    creating a backup

--- aplics/src/components/Navigation/index.jsx
+++ aplics/src/components/Navigation/index.jsx
@@ -1,45 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SignOutButton from '../SignOut';
-import * as ROUTES from '../../constants/routes';
-import { AuthUserContext } from '../Session';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import SignOutButton from '../SignOut';
+// import * as ROUTES from '../../constants/routes';
+// import { AuthUserContext } from '../Session';
 
 
-const Navigation = () => (
-  <div>
-    <AuthUserContext.Consumer>
-      {authUser =>
-        authUser ? <NavigationAuth /> : <NavigationNonAuth />
-      }
-    </AuthUserContext.Consumer>
-  </div>);
-const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
-);
-const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      {/* <Link to={ROUTES.SIGN_IN}>Sign In</Link> */}
+// const Navigation = () => (
+//   <div>
+//     <AuthUserContext.Consumer>
+//       {authUser =>
+//         authUser ? <NavigationAuth /> : <NavigationNonAuth />
+//       }
+//     </AuthUserContext.Consumer>
+//   </div>);
+// const NavigationAuth = () => (
+//   <ul>
+//     <li>
+//       <Link to={ROUTES.LANDING}>Landing</Link>
+//     </li>
+//     <li>
+//       <Link to={ROUTES.HOME}>Home</Link>
+//     </li>
+//     <li>
+//       <Link to={ROUTES.ACCOUNT}>Account</Link>
+//     </li>
+//     <li>
+//       <SignOutButton />
+//     </li>
+//   </ul>
+// );
+// const NavigationNonAuth = () => (
+//   <ul>
+//     <li>
+//       <Link to={ROUTES.LANDING}>Landing</Link>
+//     </li>
+//     <li>
+//       {/* <Link to={ROUTES.SIGN_IN}>Sign In</Link> */}
 
-    </li>
-  </ul>
-);
+//     </li>
+//   </ul>
+// );
 
 
-export default Navigation;
+// export default Navigation;

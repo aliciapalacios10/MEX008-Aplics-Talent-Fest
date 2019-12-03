25b22a6350302d1ddc493657d405c4247e07a42f
aliciapalacios10 <alizz.palacios@outlook.com>
2019-12-03 14:44:25 -0600

    maquetacion del componente de vacantes

--- src/components/Vacantes/index.jsx
+++ src/components/Vacantes/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './vacantes.css';
+import ImgVacantes from '../../img/pdf.png';
 
 const Vacantes = () => {
     return (
-        <div className="">
-    </div>
+        <div className="vacantes-section">
+            <h1 className="title">Vacantes</h1>
+            <ul className="group-png">
+                <li><img className="img-vacantes" src={ImgVacantes} /></li>
+                <li><img className="img-vacantes" src={ImgVacantes} /></li>
+                <li><img className="img-vacantes" src={ImgVacantes} /></li>
+                <li><img className="img-vacantes" src={ImgVacantes} /></li>
+            </ul>
+            <ul className="group-png">
+                <li><img className="img-vacantes" src={ImgVacantes} /></li>
+                <li><img className="img-vacantes" src={ImgVacantes} /></li>
+                <li><img className="img-vacantes" src={ImgVacantes} /></li>
+                <li><img className="img-vacantes" src={ImgVacantes} /></li>
+            </ul>
+            
+        </div>
     )
 }
 
